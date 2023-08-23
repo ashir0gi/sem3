baaaeb5202b064c7fbe7cd511483c580b7fa0698
Fedor Landyshev <fedorlandyshev@gmail.com>
2023-08-23 12:07:15 +0300

    First try1

--- lab1/MyPtrLib.h
+++ lab1/MyPtrLib.h
@@ -3,17 +3,17 @@
 template <typename T>
 class SmrtPtr {
 public:
-    SmrtPtr(T* ptr) : data(ptr), ref_count(new int(1)) {}
-    SmrtPtr(const SmrtPtr<T>& other) : data(other.data), ref_count(other.ref_count) 
+    SmrtPtr(T* ptr) : data(ptr), refCount(new int(1)) {}
+    SmrtPtr(const SmrtPtr<T>& other) : data(other.data), refCount(other.refCount) 
     {
-        (*ref_count)++;
+        (*refCount)++;
     }
     ~SmrtPtr() 
     {
-        if (--(*ref_count) == 0) // if (*ref_count == 0)
+        if (--(*refCount) == 0) // if (*refCount == 0)
         {
             delete data;
-            delete ref_count;
+            delete refCount;
         }
     }
 
@@ -21,14 +21,14 @@ public:
     {
         if (this != &other) 
         {
-            if (--(*ref_count) == 0) // if (*ref_count == 0)
+            if (--(*refCount) == 0) // if (*refCount == 0)
             {
                 delete data;
-                delete ref_count;
+                delete refCount;
             }
             data = other.data;
-            ref_count = other.ref_count;
-            (*ref_count)++;
+            refCount = other.refCount;
+            (*refCount)++;
         }
         return *this;
     }
@@ -45,5 +45,74 @@ public:
 
 private:
     T* data;         // Указатель на данные
-    int* ref_count;  // Счетчик ссылок
+    int* refCount;  // Счетчик ссылок
+};
+
+template <typename T>
+class UnqPtr;
+
+template <typename T>
+class ShrdPtr {
+public:
+    ShrdPtr(UnqPtr<T>& ptr) : data(ptr.data), refCount(ptr.refCount) 
+    {
+        (*refCount)++;
+    }
+
+    ShrdPtr(const ShrdPtr<T>& other) : data(other.data), refCount(other.refCount) 
+    {
+        (*refCount)++;
+    }
+
+    ~ShrdPtr() 
+    {
+        if (--(*refCount) == 0) 
+        {
+            delete data;
+            delete refCount;
+        }
+    }
+
+    T& operator*() const 
+    {
+        return *data;
+    }
+
+    T* operator->() const 
+    {
+        return data;
+    }
+
+private:
+    T* data;         // Указатель на данные
+    int* refCount;  // Счетчик ссылок
+
+    friend class UnqPtr<T>;
+};
+
+template <typename T>
+class UnqPtr {
+public:
+    UnqPtr(T* ptr) : data(ptr), refCount(new int(1)) {}
+
+    UnqPtr(ShrdPtr<T>&& other) : data(other.data), refCount(other.refCount) 
+    {
+        other.data = nullptr;
+        other.refCount = nullptr;
+    }
+
+    ~UnqPtr() 
+    {
+        if (--(*refCount) == 0) 
+        {
+            delete data;
+            delete refCount;
+        }
+    }
+
+private:
+    T* data;         // Указатель на данные
+    int* refCount;  // Счетчик ссылок
+
+    friend class ShrdPtr<T>;
 };
